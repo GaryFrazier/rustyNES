dea3e3e49d59d6267575a47a93f6a742c2bc6e34
Gary Frazier <garyfrazier95@yahoo.com>
2022-06-24 00:44:34 -0400

    log

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -86,7 +86,7 @@ fn run_next_instruction(emulator: &mut config::Emulator) {
     let mut opcode_iterator = instructions::OPCODES.iter();
 
     // we unwrap the find here so it crashes if the opcode is invalid, for now
-    println!("{}", opcode);
+    println!("{:#04x}", opcode);
 
     // for nestest
     let error_code = ram::read_u8(mapped_address, &mut emulator.cpu.memory, 0x2);
